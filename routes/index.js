ee084dedec4758f5e538b5934fe8bc8265025230
Santhi Kumar Yadavalli <santhikumaryadavalli23@gmail.com>
2025-04-04 10:33:33 +0530

    feat: add route for /

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,9 @@ const router = Router();
 router.use("/user", userRouter);
 router.use("/habit" , habitRouter);
 router.use("/user-habit" , userHabitRoute);
+
+router.get("", (req, res) => {
+  res.send("Welcome!").status(200);
+});
+
 export default router;
